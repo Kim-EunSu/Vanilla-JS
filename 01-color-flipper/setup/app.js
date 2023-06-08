2dbe562c4449164fa9ef57b9891ef7a784a8a83d
Kim-EunSu <kes73640@gmail.com>
2023-06-08 10:43:56 +0900

    0608

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,20 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function () {
+  console.log("click");
+  //  randomNumber은  0 ~ 3까지
+  //const randomNumber = 2;
+
+  const randomNumber = getRandomNumber();
+  console.log(randomNumber);
+
+  document.body.style.backgroundColor = colors[randomNumber];
+  color.textContent = colors[randomNumber];
+});
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * colors.length);
+}
